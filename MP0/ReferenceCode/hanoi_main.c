6c1c442060248e5adc1479acf2dc08e5a957a45f
Chi-Sheng (Daniel) Shih <chisheng.shih@gmail.com>
2015-09-02 15:55:09 +0800

    Revise makefile examples

--- MP0/ReferenceCode/hanoi_main.c
+++ MP0/ReferenceCode/hanoi_main.c
@@ -1,15 +1,23 @@
+//
+//  main.c
+//  Hanoi
+//
+//  Created by Chi-Sheng Shih on 9/2/15.
+//  Copyright (c) 2015 Chi-Sheng Shih. All rights reserved.
+//
+
 #include <stdio.h>
 #include <stdlib.h>
 #include "hanoi.h"
 
-int main(int argc, char** argv) {
-	if (argc < 2) {
-		printf("Usage: hanoi n.\n");
-		return -1;
-	} else {
-		int n = atoi(argv[1]);
-		long res = hanoi(n, 0, 2);
-		printf("%ld\n", res);
-		return 0;
-	}
+int main(int argc, const char * argv[]) {
+    if (argc < 2) {
+        printf("Usage: hanoi n.\n");
+        return -1;
+    } else {
+        int n = atoi(argv[1]);
+        long res = hanoi(n, 0, 2);
+        printf("%ld\n", res);
+        return 0;
+    }
 }
